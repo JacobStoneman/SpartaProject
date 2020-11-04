a59b4b49fb6c01db0167ace470984d9aa322e15f
Jacob Stoneman (stonemaj) <jacob679@hotmail.co.uk>
2020-11-04 20:41:41 +0000

    Configured multiple pages

--- SpartaProject/SpartaProjectGUI/LoginWindow.xaml.cs
+++ SpartaProject/SpartaProjectGUI/LoginWindow.xaml.cs
@@ -15,14 +15,17 @@ using System.Windows.Shapes;
 
 namespace SpartaProjectGUI
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+	public partial class LoginWindow : Window
 	{
-		public MainWindow()
+		public LoginWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void button_Click(object sender, RoutedEventArgs e)
+		{
+			CustomerWindow win = new CustomerWindow();
+			Content = win;
 		}
 	}
 }
