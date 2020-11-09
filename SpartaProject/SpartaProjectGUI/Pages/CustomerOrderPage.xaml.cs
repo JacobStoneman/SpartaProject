4b7524060641e58a7d3994a6f9defa2dc39298d8
Jacob Stoneman (stonemaj) <jacob679@hotmail.co.uk>
2020-11-09 18:07:42 +0000

    Customer functionality complete

--- SpartaProject/SpartaProjectGUI/Pages/CustomerOrderPage.xaml.cs
+++ SpartaProject/SpartaProjectGUI/Pages/CustomerOrderPage.xaml.cs
@@ -40,8 +40,71 @@ namespace SpartaProjectGUI.Pages
 			{
 				Customer currentCustomer = db.Customers.Where(c => c.UserId == CrudUser.Selected.UserId).FirstOrDefault();
 				List<Order> customerOrders = db.Orders.Where(o => o.CustomerId == currentCustomer.CustomerId).ToList();
-				listBox_myOrders.ItemsSource = customerOrders;
+				List<OrderItem> listItems = new List<OrderItem>();
+
+				foreach(Order o in customerOrders)
+				{
+					if (o.Shipped)
+					{
+						BitmapImage newImage = new BitmapImage(new Uri(@"\Images\shipped.png", UriKind.Relative));
+						listItems.Add(new OrderItem() {Order = o, OrderDetails = o.ToString(), OrderStatus = newImage });
+					} else
+					{
+						listItems.Add(new OrderItem() {Order = o, OrderDetails = o.ToString(), OrderStatus = null });
+					}
+				}
+
+				listBox_myOrders.ItemsSource = listItems;
 			}
 		}
+
+		private void SetSelectedProductGrid()
+		{
+			if (CrudOrder.Selected == null)
+			{
+				logic.InitialiseProductInfoGrid(textBlock_product_id_value, textBlock_product_name_value, textBlock_product_price_value, textBlock_product_rating_value, image_product);
+			}
+			else
+			{
+				using (ProjectContext db = new ProjectContext())
+				{
+					Product selectedProduct = db.Products.Where(p => p.ProductId == CrudOrder.Selected.ProductId).FirstOrDefault();
+					logic.DisplayProductInfoGrid(selectedProduct, textBlock_product_id_value, textBlock_product_name_value, textBlock_product_price_value, textBlock_product_rating_value, image_product);
+				}
+			}
+		}
+
+		private void button_deleteOrder_Click(object sender, RoutedEventArgs e)
+		{
+			if (CrudOrder.Selected.Shipped)
+			{
+				MessageBox.Show("This order has been shipped and can no longer be cancelled");
+			}
+			else
+			{
+				CrudOrder.Delete(CrudOrder.Selected.OrderId);
+				CrudOrder.Selected = null;
+				SetSelectedProductGrid();
+				PopulateOrderList();
+			}
+		}
+
+		private void listBox_myOrders_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (listBox_myOrders.SelectedItem != null)
+			{
+				OrderItem selectedItem = listBox_myOrders.SelectedItem as OrderItem;
+				CrudOrder.Selected = CrudOrder.SetSelected<Order>(selectedItem.Order);
+				SetSelectedProductGrid();
+			}
+		}
+	}
+
+	//Used to Set individual elements of listbox item
+	public class OrderItem
+	{
+		public Order Order { get; set; }
+		public string OrderDetails { get; set; }
+		public ImageSource OrderStatus { get; set; }
 	}
 }
